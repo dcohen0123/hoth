4c9fee0cb19c139d19aac26109f04e338a95400d
Michael Cohen <michaelcohen@Michaels-iMac.local>
2022-05-10 10:37:37 -0400

    Add title / type to dashboard.

--- src/interface/IDashboard.ts
+++ src/interface/IDashboard.ts
@@ -2,6 +2,8 @@ import { IInput } from "./IInput";
 import { IWidget } from "./IWidget";
 
 export interface IDashboard {
+    id: string;
+    title: string;
     inputs: IInput[];
     widgets: IWidget[];
 }
