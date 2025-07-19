2ec98bb3c5bfb6c3909a50f62fa1edbc3424de8a
Michael Cohen <michaelcohen@Michaels-iMac.local>
2025-07-19 09:52:04 -0400

    Update

--- src/redux/Workspace/WorkspaceSaga.ts
+++ src/redux/Workspace/WorkspaceSaga.ts
@@ -6,7 +6,6 @@ export function* initWorkspaceHandler() {
     try {
         // TODO - Fetch workspaces and populate list of workspaces / set default workspace
         yield put({type: SelectWorkspace, payload: {id: "workspace", name: "Workspace", layout: null, views: []}});
-        yield put({type: AddView, payload: {id: 'workflow', name: 'Workflow', type: ViewType.WorkflowManager}});
     } catch(e) {
         console.error(e);
     }
